588e4de955612b767b980a9cd1bbd78b672375dd
Azam2111 <167036016+Azam2111@users.noreply.github.com>
2026-09-08 23:52:54 +0300

    API kaliti tuzatildi (noto'g'ri ko'chirilgan edi), v13

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@
 // Ma'lumotni himoya qiladigan narsa - Firestore Rules: har kim faqat
 // o'zining users/{uid}/... yo'liga kira oladi.
 window.REJAM_FIREBASE = {
-  apiKey: "AIzaSyAOL9-B1w9zqtNmgA87xhxlh0cAoov_oqDg",
+  apiKey: "AIzaSyAL9-B1w9zqtNmgA87xhxlh0cAoov_oqDg",
   authDomain: "rejam-2d9f1.firebaseapp.com",
   projectId: "rejam-2d9f1",
   storageBucket: "rejam-2d9f1.firebasestorage.app",
